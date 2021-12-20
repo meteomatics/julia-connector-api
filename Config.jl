524c5ae34ef5ec9de7b94867d8e9dfc9610ab80c
EagerTom <96126171+EagerTom@users.noreply.github.com>
2021-12-20 10:07:20 +0100

    Delete Config.jl

--- Config.jl
+++
@@ -1,5 +0,0 @@
-# file for containing constants required for using MeteomaticsAPI.
-# currently limited to user credentials (which can be overridden in a function call)
-
-username = "python-community"
-password = "Umivipawe179"
